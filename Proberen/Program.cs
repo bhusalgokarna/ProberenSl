946870e905e057aa8296016c016844192d6a4103
bhusa <bhusa@LAPTOP-VOJLBL85>
2022-09-27 15:51:15 +0200

    Add project files.

---
+++ Proberen/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proberen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal givenNumber = 0;
+            Console.Write(" Give a number: ");
+            givenNumber = decimal.Parse(Console.ReadLine());
+           
+            decimal geteldNumber = givenNumber + 15;
+            
+            decimal vermenigvuldigdNumber = geteldNumber * 2;
+            decimal afgetrokkenNumber = vermenigvuldigdNumber - 4;
+            decimal divideNumber = afgetrokkenNumber / 2;
+            Console.WriteLine($"The different between given number and laste result is:{divideNumber-givenNumber}");
+
+           // Console.OutputEncoding = Encoding.UTF8;
+           // Console.Write("give een nummer: ");
+            //decimal price = decimal.Parse(Console.ReadLine());
+           // Console.WriteLine(price);
+            //Console.WriteLine($"price is equal to {price,0:C2}");
+            Console.ReadLine();
+
+
+        }
+    }
+}
